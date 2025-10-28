5dce137b899533f3b6f7a72b7d5ff53c5a35d60f
harsh pandya <harsh.pandya@vrinsoft.com>
2025-10-28 16:59:36 +0530

    Updated

--- Main.py
+++ Main.py
@@ -14,6 +14,6 @@ df.info()
 df.drop_duplicates(inplace=True)
 df.dropna(inplace=True)
 
-df.drop(['student_id'], axis=1, inplace=True)
+# df.drop(['student_id'], axis=1, inplace=True)
 df = pd.get_dummies(df, drop_first=True)
 df.head()
